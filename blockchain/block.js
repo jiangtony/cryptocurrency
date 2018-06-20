699cc3de1dd9e9671502624378b8480247ca85db
Tony Jiang <tony.jiang34@gmail.com>
2018-06-20 16:46:19 -0400

    variable mine rate and difficulty

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,15 +1,16 @@
 const SHA256 = require('crypto-js/sha256');
-const { DIFFICULTY } = require('../config');
+const { DIFFICULTY, MINE_RATE } = require('../config');
 
 // Block class
 
 class Block{
-	constructor(timestamp, prevHash, hash, data, nonce) {
+	constructor(timestamp, prevHash, hash, data, nonce, difficulty) {
 		this.timestamp = timestamp;
 		this.prevHash = prevHash;
 		this.hash = hash;
 		this.data = data;
 		this.nonce = nonce;
+		this.difficulty = difficulty || DIFFICULTY;
 	}
 
 	toString() {
@@ -18,12 +19,13 @@ class Block{
 			Previous Hash: ${this.prevHash}
 			Hash         : ${this.hash}
 			Data         : ${this.data}
-			Nonce        : ${this.nonce}`;
+			Nonce        : ${this.nonce}
+			Diffculty    : ${this.difficulty}`;
 	}
 
 	// Genesis block
 	static genesis() {
-		return new this('genesis', 'none', 'first', [], 0);
+		return new this('genesis', 'none', 'first', [], 0, DIFFICULTY);
 	}
 
 	// Creates a new block
@@ -37,18 +39,22 @@ class Block{
 		 */
 		let hash, timestamp;
 		let nonce = 0;
+
+		// Difficulty of lastBlock
+		let { difficulty } = lastBlock;
 		do {
 			nonce++;
 			timestamp = Date.now();
-			hash = Block.hash(timestamp, prevHash, data, nonce);
-		} while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+			difficulty =  Block.adjustDifficulty(lastBlock, timestamp);
+			hash = Block.hash(timestamp, prevHash, data, nonce, difficulty);
+		} while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
-		return new this(timestamp, prevHash, hash, data, nonce);
+		return new this(timestamp, prevHash, hash, data, nonce, difficulty);
 	}
 
 	// Generate SHA-256 hash
-	static hash(timestamp, prevHash, data, nonce) {
-		return SHA256(`${timestamp}${prevHash}${data}${nonce}`).toString();
+	static hash(timestamp, prevHash, data, nonce, difficulty) {
+		return SHA256(`${timestamp}${prevHash}${data}${nonce}${difficulty}`).toString();
 	}
 
 	/**
@@ -56,8 +62,18 @@ class Block{
 	 * if the block has not been tampered with
 	 */
 	static checkHash(block) {
-		const {timestamp, prevHash, data, nonce} = block;
-		return Block.hash(timestamp, prevHash, data, nonce);
+		const {timestamp, prevHash, data, nonce, difficulty} = block;
+		return Block.hash(timestamp, prevHash, data, nonce, difficulty);
+	}
+
+	/**
+	 * Compares the currentTime to the timestamp of the previous block
+	 * If the difference is less than the MINE_RATE, increase difficulty
+	 */
+	static adjustDifficulty(lastBlock, currentTime) {
+		let { difficulty } = lastBlock;
+		difficulty = lastBlock.timestamp + MINE_RATE > currentTime ? difficulty + 1 : difficulty -1;
+		return difficulty;
 	}
 }
 
--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+// Number of leading zero bits for proof of work
 const DIFFICULTY = 4;
 
-module.exports = { DIFFICULTY };
+// Rate at which blocks are mined in milliseconds
+const MINE_RATE = 5000;
+
+module.exports = { DIFFICULTY, MINE_RATE };
