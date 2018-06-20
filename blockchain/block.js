5b54909a4337287f546962004a8486bf8abea7d8
Tony Jiang <tony.jiang34@gmail.com>
2018-06-20 14:29:38 -0400

    proof of work implementation

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,39 +1,54 @@
 const SHA256 = require('crypto-js/sha256');
+const { DIFFICULTY } = require('../config');
 
 // Block class
 
 class Block{
-	constructor(timestamp, prevHash, hash, data) {
+	constructor(timestamp, prevHash, hash, data, nonce) {
 		this.timestamp = timestamp;
 		this.prevHash = prevHash;
 		this.hash = hash;
 		this.data = data;
+		this.nonce = nonce;
 	}
 
 	toString() {
 		return `Block -
-			Timestamp: ${this.timestamp}
+			Timestamp    : ${this.timestamp}
 			Previous Hash: ${this.prevHash}
-			Hash: ${this.hash}
-			Data: ${this.data}`;
+			Hash         : ${this.hash}
+			Data         : ${this.data}
+			Nonce        : ${this.nonce}`;
 	}
 
 	// Genesis block
 	static genesis() {
-		return new this('genesis', 'none', 'first', []);
+		return new this('genesis', 'none', 'first', [], 0);
 	}
 
 	// Creates a new block
 	static mineBlock(lastBlock, data) {
-		const timestamp = Date.now();
 		const prevHash = lastBlock.hash;
-		const hash = Block.hash(timestamp, prevHash, data);
-		return new this(timestamp, prevHash, hash, data);
+
+		/**
+		 * Proof of work
+		 * Generate new hashes until a hash is found with a leading number of
+		 * zero bits equal to the difficulty
+		 */
+		let hash, timestamp;
+		let nonce = 0;
+		do {
+			nonce++;
+			timestamp = Date.now();
+			hash = Block.hash(timestamp, prevHash, data, nonce);
+		} while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+
+		return new this(timestamp, prevHash, hash, data, nonce);
 	}
 
 	// Generate SHA-256 hash
-	static hash(timestamp, prevHash, data) {
-		return SHA256(`${timestamp}${prevHash}${data}`).toString();
+	static hash(timestamp, prevHash, data, nonce) {
+		return SHA256(`${timestamp}${prevHash}${data}${nonce}`).toString();
 	}
 
 	/**
@@ -41,8 +56,8 @@ class Block{
 	 * if the block has not been tampered with
 	 */
 	static checkHash(block) {
-		const {timestamp, prevHash, data} = block;
-		return Block.hash(timestamp, prevHash, data);
+		const {timestamp, prevHash, data, nonce} = block;
+		return Block.hash(timestamp, prevHash, data, nonce);
 	}
 }
 
